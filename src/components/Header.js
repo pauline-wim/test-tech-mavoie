4ae470aab0d150bb86ba83fda93b7a60e10e9e4c
pauline-wim <linette.wim@hotmail.com>
2022-02-27 12:21:25 +0100

    Creating Header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import { Icon } from "@iconify/react";
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <Icon icon="bxs:store" className="store-logo" />
+      <h1>Ma boutique</h1>
+    </header>
+  );
+}
